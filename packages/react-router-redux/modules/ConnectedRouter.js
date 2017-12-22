70f64022c120878b7d302083fe7cca936ccfba00
vicki <vicki@thenetcircle.com>
2017-12-22 15:00:22 +0800

    download

---
+++ packages/react-router-redux/modules/ConnectedRouter.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Router } from 'react-router'
+
+import { LOCATION_CHANGE } from './reducer'
+
+class ConnectedRouter extends Component {
+  static propTypes = {
+    store: PropTypes.object,
+    history: PropTypes.object.isRequired,
+    children: PropTypes.node,
+    isSSR: PropTypes.bool
+  }
+
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  handleLocationChange = location => {
+    this.store.dispatch({
+      type: LOCATION_CHANGE,
+      payload: location
+    })
+  }
+
+  componentWillMount() {
+    const { store:propsStore, history, isSSR } = this.props
+    this.store = propsStore || this.context.store
+
+    if (!isSSR)
+      this.unsubscribeFromHistory = history.listen(this.handleLocationChange)
+
+    this.handleLocationChange(history.location)
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribeFromHistory) this.unsubscribeFromHistory()
+  }
+
+  render() {
+    return <Router {...this.props} />
+  }
+}
+
+export default ConnectedRouter
